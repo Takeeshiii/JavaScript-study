448d82e3a66d3567ff13452bf7631246324d339f
Takeeshiii <panda3413@icloud.com>
2022-07-12 22:43:50 +0900

    イベント発火 基礎編

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists);
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists")
+
+pullDownButton.addEventListener('mouseover',function(){
+    console.log("noru");
+    })
+
+pullDownButton.addEventListener('mouseout',function(){
+    console.log("hazureru");
+    })
+
+pullDownButton.addEventListener('click',function(){
+    console.log("osu");
+    })
+})
