1982ca2d85456850a789ea9d633fc098fbcc93b7
Takeeshiii <panda3413@icloud.com>
2022-07-18 21:18:43 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists);
 window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists")
+    const pullDownParents = document.getElementById("pull-down")
 
 pullDownButton.addEventListener('mouseover',function(){
     console.log("noru　色は青");
-    pullDownButton.setAttribute("style","background-color:blue;")
+    this.setAttribute("style","background-color:blue;")
     })
 
 pullDownButton.addEventListener('mouseout',function(){
     console.log("hazureru　色は赤");
-    pullDownButton.setAttribute("style","background-color:red;")
+    this.setAttribute("style","background-color:red;")
     })
 
 pullDownButton.addEventListener('click',function(){
     console.log("osu　色は緑");
-    pullDownButton.setAttribute("style","background-color:green;")
+    this.setAttribute("style","background-color:green;")
+    if(pullDownParents.getAttribute("style") == "display:block;"){
+        this.removeAttribute("style","display:block;")
+        console.log("非表示")
+    }else{
+        this.setAttribute("style","display:block;")
+        console.log("表示")
+    }
     })
 })
