42310401e5ba2472af4310cb399d12d88ecd0e2a
Takeeshiii <panda3413@icloud.com>
2022-07-22 22:51:40 +0900

    文字列の取得編

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+function pullDown(){
 let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists);
-window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists")
     const pullDownParents = document.getElementById("pull-down")
-
-pullDownButton.addEventListener('mouseover',function(){
-    console.log("noru　色は青");
-    this.setAttribute("style","background-color:blue;")
+    const pullDownChaild = document.querySelectorAll(".pull-down-list")
+    const currentList = document.getElementById("current-list")
+    pullDownButton.addEventListener('mouseover', function () {
+        console.log("noru　色は青");
+        this.setAttribute("style", "background-color:blue;")
     })
-
-pullDownButton.addEventListener('mouseout',function(){
-    console.log("hazureru　色は赤");
-    this.setAttribute("style","background-color:red;")
+    pullDownButton.addEventListener('mouseout', function () {
+        console.log("hazureru　色は赤");
+        this.setAttribute("style", "background-color:red;")
     })
-
-pullDownButton.addEventListener('click',function(){
-    console.log("osu　色は緑");
-    this.setAttribute("style","background-color:green;")
-    if(pullDownParents.getAttribute("style") == "display:block;"){
-        this.removeAttribute("style","display:block;")
-        console.log("非表示")
-    }else{
-        this.setAttribute("style","display:block;")
-        console.log("表示")
-    }
+    pullDownButton.addEventListener('click', function () {
+        console.log("osu　色は緑");
+        this.setAttribute("style", "background-color:green;")
+        if (pullDownParents.getAttribute("style") == "display:block;") {
+            pullDownParents.removeAttribute("style", "display:block;")
+            console.log("非表示")
+        } else {
+            pullDownParents.setAttribute("style", "display:block;")
+            console.log("表示")
+        }
     })
-})
+    pullDownChaild.forEach(function (list) {
+        list.addEventListener('click', function () {
+            const value = list.innerHTML
+            currentList.innerHTML = value
+            console.log(value)
+        })
+    })
+}
+window.addEventListener('load',pullDown)
